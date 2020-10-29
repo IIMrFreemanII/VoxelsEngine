e5436597bb2a1b1188e1a3535ce50b307af9e551
IIMrFreemanII <kdeagovec@gmail.com>
2020-10-29 22:04:31 +0200

    implement storing voxels chunk in scriptable object

---
+++ Assets/VoxelsEngine/Voxels/Scripts/VoxelsChunk.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+
+namespace VoxelsEngine.Voxels.Scripts
+{
+    [CreateAssetMenu(fileName = "Voxels Chunk", menuName = "Voxels Engine/Voxels Chunk")]
+    public class VoxelsChunk : ScriptableObject
+    {
+        VoxelData[,,] _data =
+        {
+            // x
+            {
+                // y
+                {
+                    //z
+                    null, new VoxelData(Color.blue), new VoxelData(Color.blue)
+                },
+                {
+                    new VoxelData(Color.blue), new VoxelData(Color.blue), new VoxelData(Color.blue)
+                },
+                {
+                    new VoxelData(Color.blue), new VoxelData(Color.blue), null
+                }
+            },
+            {
+                {
+                    null, new VoxelData(Color.blue), new VoxelData(Color.blue)
+                },
+                {
+                    new VoxelData(Color.blue), new VoxelData(Color.blue), new VoxelData(Color.blue)
+                },
+                {
+                    new VoxelData(Color.blue), new VoxelData(Color.blue), null
+                }
+            },
+            {
+                {
+                    null, new VoxelData(Color.blue), new VoxelData(Color.blue)
+                },
+                {
+                    new VoxelData(Color.blue), new VoxelData(Color.blue), new VoxelData(Color.blue)
+                },
+                {
+                    new VoxelData(Color.blue), new VoxelData(Color.blue), null
+                }
+            },
+        };
+
+        public int Width => _data.GetLength(0);
+        public int Height => _data.GetLength(1);
+        public int Depth => _data.GetLength(2);
+
+        public VoxelData GetCell(int x, int y, int z)
+        {
+            return _data[x, y, z];
+        }
+
+        public VoxelData GetNeighbor(int x, int y, int z, Direction dir)
+        {
+            DataCoordinate offsetToCheck = _offsets[(int) dir];
+            DataCoordinate neighborCoord =
+                new DataCoordinate(x + offsetToCheck.x, y + offsetToCheck.y, z + offsetToCheck.z);
+
+            if (
+                neighborCoord.x < 0 || neighborCoord.x >= Width ||
+                neighborCoord.y < 0 || neighborCoord.y >= Height ||
+                neighborCoord.z < 0 || neighborCoord.z >= Depth
+            )
+            {
+                return null;
+            }
+
+            return GetCell(neighborCoord.x, neighborCoord.y, neighborCoord.z);
+        }
+
+        private readonly DataCoordinate[] _offsets =
+        {
+            new DataCoordinate(0, 0, 1),
+            new DataCoordinate(1, 0, 0),
+            new DataCoordinate(0, 0, -1),
+            new DataCoordinate(-1, 0, 0),
+            new DataCoordinate(0, 1, 0),
+            new DataCoordinate(0, -1, 0),
+        };
+
+        struct DataCoordinate
+        {
+            public int x;
+            public int y;
+            public int z;
+
+            public DataCoordinate(int x, int y, int z)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+        }
+    }
+
+    public enum Direction
+    {
+        North,
+        East,
+        South,
+        West,
+        Up,
+        Down
+    }
+}
---
+++ Assets/VoxelsEngine/Voxels/Scripts/VoxelsChunkRenderer.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace VoxelsEngine.Voxels.Scripts
+{
+    [RequireComponent(typeof(MeshFilter)), RequireComponent(typeof(MeshRenderer))]
+    public class VoxelsChunkRenderer : MonoBehaviour
+    {
+        public VoxelsChunk voxelsChunk;
+        public float scale = 1f;
+
+        private float _adjustedScale;
+
+        private Mesh _mesh;
+        private List<Vector3> _vertices;
+        private List<int> _triangles;
+
+        private void Awake()
+        {
+            _mesh = GetComponent<MeshFilter>().mesh;
+            _adjustedScale = scale * 0.5f;
+        }
+
+        private void Start()
+        {
+            if (!voxelsChunk)
+            {
+                Debug.LogWarning("There is not voxels chunk to render!");
+                return;
+            }
+            
+            GenerateVoxelsMesh(voxelsChunk);
+            UpdateMesh();
+        }
+
+        private void GenerateVoxelsMesh(VoxelsChunk data)
+        {
+            _vertices = new List<Vector3>();
+            _triangles = new List<int>();
+        
+            for (int x = 0; x < data.Width; x++)
+            {
+                for (int y = 0; y < data.Height; y++)
+                {
+                    for (int z = 0; z < data.Depth; z++)
+                    {
+                        if (data.GetCell(x, y, z) == null) continue;
+                
+                        MakeCube(_adjustedScale, new Vector3(x, y, z) * scale, x, y, z, data);
+                    }
+                }
+            }
+        }
+
+        private void MakeCube(float scale, Vector3 cubePos, int x, int y, int z, VoxelsChunk data)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                if (data.GetNeighbor(x, y, z, (Direction) i) == null)
+                    MakeFace((Direction)i, scale, cubePos);
+            }
+        }
+
+        private void MakeFace(Direction dir, float scale, Vector3 facePos)
+        {
+            _vertices.AddRange(VoxelMeshData.FaceVertices(dir, scale, facePos));
+
+            int vertCount = _vertices.Count;
+
+            _triangles.Add(vertCount - 4);
+            _triangles.Add(vertCount - 4 + 1);
+            _triangles.Add(vertCount - 4 + 2);
+            _triangles.Add(vertCount - 4);
+            _triangles.Add(vertCount - 4 + 2);
+            _triangles.Add(vertCount - 4 + 3);
+        }
+
+        private void UpdateMesh()
+        {
+            _mesh.Clear();
+
+            _mesh.vertices = _vertices.ToArray();
+            _mesh.triangles = _triangles.ToArray();
+
+            _mesh.RecalculateNormals();
+        }
+    }
+}
