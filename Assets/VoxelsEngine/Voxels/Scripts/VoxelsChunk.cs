ab6d6260884696e04fd5ee6edf5c06620f5e4916
IIMrFreemanII <kdeagovec@gmail.com>
2020-10-30 23:50:36 +0200

    implement DrawWireRect; implement storing voxels chunk data in scriptable object;

--- Assets/VoxelsEngine/Voxels/Scripts/VoxelsChunk.cs
+++ Assets/VoxelsEngine/Voxels/Scripts/VoxelsChunk.cs
@@ -5,60 +5,72 @@ namespace VoxelsEngine.Voxels.Scripts
     [CreateAssetMenu(fileName = "Voxels Chunk", menuName = "Voxels Engine/Voxels Chunk")]
     public class VoxelsChunk : ScriptableObject
     {
-        VoxelData[,,] _data =
-        {
-            // x
-            {
-                // y
-                {
-                    //z
-                    null, new VoxelData(Color.blue), new VoxelData(Color.blue)
-                },
-                {
-                    new VoxelData(Color.blue), new VoxelData(Color.blue), new VoxelData(Color.blue)
-                },
-                {
-                    new VoxelData(Color.blue), new VoxelData(Color.blue), null
-                }
-            },
-            {
-                {
-                    null, new VoxelData(Color.blue), new VoxelData(Color.blue)
-                },
-                {
-                    new VoxelData(Color.blue), new VoxelData(Color.blue), new VoxelData(Color.blue)
-                },
-                {
-                    new VoxelData(Color.blue), new VoxelData(Color.blue), null
-                }
-            },
-            {
-                {
-                    null, new VoxelData(Color.blue), new VoxelData(Color.blue)
-                },
-                {
-                    new VoxelData(Color.blue), new VoxelData(Color.blue), new VoxelData(Color.blue)
-                },
-                {
-                    new VoxelData(Color.blue), new VoxelData(Color.blue), null
-                }
-            },
-        };
+        public Vector3Int size = new Vector3Int(3, 3, 3);
+
+        private VoxelData[,,] _data;
+        // {
+        //     {
+        //         {
+        //             new VoxelData(Color.black), new VoxelData(Color.black), new VoxelData(Color.black),
+        //         },
+        //         {
+        //             new VoxelData(Color.black), new VoxelData(Color.black), new VoxelData(Color.black),
+        //         },
+        //         {
+        //             new VoxelData(Color.black), new VoxelData(Color.black), new VoxelData(Color.black),
+        //         },
+        //     },
+        //     {
+        //         {
+        //             new VoxelData(Color.black), new VoxelData(Color.black), new VoxelData(Color.black),
+        //         },
+        //         {
+        //             new VoxelData(Color.black), new VoxelData(Color.black), new VoxelData(Color.black),
+        //         },
+        //         {
+        //             new VoxelData(Color.black), new VoxelData(Color.black), new VoxelData(Color.black),
+        //         },
+        //     },
+        //     {
+        //         {
+        //             new VoxelData(Color.black), new VoxelData(Color.black), new VoxelData(Color.black),
+        //         },
+        //         {
+        //             new VoxelData(Color.black), new VoxelData(Color.black), new VoxelData(Color.black),
+        //         },
+        //         {
+        //             new VoxelData(Color.black), new VoxelData(Color.black), new VoxelData(Color.black),
+        //         },
+        //     }
+        // };
 
         public int Width => _data.GetLength(0);
         public int Height => _data.GetLength(1);
         public int Depth => _data.GetLength(2);
+
+        private void OnValidate()
+        {
+            size.x = size.x >= 1 ? size.x : 1;
+            size.y = size.y >= 1 ? size.y : 1;
+            size.z = size.z >= 1 ? size.z : 1;
+
+            _data = new VoxelData[size.x, size.y, size.z];
+        }
 
         public VoxelData GetCell(int x, int y, int z)
         {
             return _data[x, y, z];
         }
 
-        public VoxelData GetNeighbor(int x, int y, int z, Direction dir)
+        public void SetSell(VoxelData data, Vector3Int position)
         {
-            DataCoordinate offsetToCheck = _offsets[(int) dir];
-            DataCoordinate neighborCoord =
-                new DataCoordinate(x + offsetToCheck.x, y + offsetToCheck.y, z + offsetToCheck.z);
+            _data[position.x, position.y, position.z] = data;
+        }
+
+        public VoxelData GetNeighbor(Vector3Int coordinate, Direction dir)
+        {
+            Vector3Int offsetToCheck = _offsets[(int) dir];
+            Vector3Int neighborCoord = coordinate + offsetToCheck;
 
             if (
                 neighborCoord.x < 0 || neighborCoord.x >= Width ||
@@ -72,29 +84,15 @@ namespace VoxelsEngine.Voxels.Scripts
             return GetCell(neighborCoord.x, neighborCoord.y, neighborCoord.z);
         }
 
-        private readonly DataCoordinate[] _offsets =
+        private readonly Vector3Int[] _offsets =
         {
-            new DataCoordinate(0, 0, 1),
-            new DataCoordinate(1, 0, 0),
-            new DataCoordinate(0, 0, -1),
-            new DataCoordinate(-1, 0, 0),
-            new DataCoordinate(0, 1, 0),
-            new DataCoordinate(0, -1, 0),
+            new Vector3Int(0, 0, 1),
+            new Vector3Int(1, 0, 0),
+            new Vector3Int(0, 0, -1),
+            new Vector3Int(-1, 0, 0),
+            new Vector3Int(0, 1, 0),
+            new Vector3Int(0, -1, 0),
         };
-
-        struct DataCoordinate
-        {
-            public int x;
-            public int y;
-            public int z;
-
-            public DataCoordinate(int x, int y, int z)
-            {
-                this.x = x;
-                this.y = y;
-                this.z = z;
-            }
-        }
     }
 
     public enum Direction
--- Assets/VoxelsEngine/Voxels/Scripts/VoxelsChunkRenderer.cs
+++ Assets/VoxelsEngine/Voxels/Scripts/VoxelsChunkRenderer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using VoxelsEngine.Extensions;
 
 namespace VoxelsEngine.Voxels.Scripts
 {
@@ -21,6 +22,11 @@ namespace VoxelsEngine.Voxels.Scripts
             _adjustedScale = scale * 0.5f;
         }
 
+        private void OnValidate()
+        {
+            _adjustedScale = scale * 0.5f;
+        }
+
         private void Start()
         {
             if (!voxelsChunk)
@@ -28,16 +34,48 @@ namespace VoxelsEngine.Voxels.Scripts
                 Debug.LogWarning("There is not voxels chunk to render!");
                 return;
             }
-            
+
             GenerateVoxelsMesh(voxelsChunk);
             UpdateMesh();
+        }
+
+        public bool TestGizmo;
+        private void OnDrawGizmosSelected()
+        {
+            if (!voxelsChunk) return;
+
+            Gizmos.color = Color.white;
+
+            if (TestGizmo)
+            {
+                GizmosExtension.DrawWireRect(
+                    transform.position,
+                    new Vector3(voxelsChunk.Width, voxelsChunk.Height, voxelsChunk.Depth) * scale
+                );
+            }
+            else
+            {
+                for (int x = 0; x < voxelsChunk.Width; x++)
+                {
+                    for (int y = 0; y < voxelsChunk.Height; y++)
+                    {
+                        for (int z = 0; z < voxelsChunk.Depth; z++)
+                        {
+                            Vector3 cubePos = new Vector3(x, y, z) * scale;
+                            Vector3 offset = Vector3.one * scale * 0.5f;
+
+                            Gizmos.DrawWireCube(cubePos + offset, Vector3.one * scale);
+                        }
+                    }
+                }
+            }
         }
 
         private void GenerateVoxelsMesh(VoxelsChunk data)
         {
             _vertices = new List<Vector3>();
             _triangles = new List<int>();
-        
+
             for (int x = 0; x < data.Width; x++)
             {
                 for (int y = 0; y < data.Height; y++)
@@ -45,19 +83,27 @@ namespace VoxelsEngine.Voxels.Scripts
                     for (int z = 0; z < data.Depth; z++)
                     {
                         if (data.GetCell(x, y, z) == null) continue;
-                
-                        MakeCube(_adjustedScale, new Vector3(x, y, z) * scale, x, y, z, data);
+
+                        Vector3 cubePos = new Vector3(x, y, z) * scale;
+                        Vector3 offset = Vector3.one * scale * 0.5f;
+
+                        MakeCube(
+                            _adjustedScale,
+                            cubePos + offset,
+                            new Vector3Int(x, y, z),
+                            data
+                        );
                     }
                 }
             }
         }
 
-        private void MakeCube(float scale, Vector3 cubePos, int x, int y, int z, VoxelsChunk data)
+        private void MakeCube(float scale, Vector3 cubePos, Vector3Int coordinate, VoxelsChunk data)
         {
             for (int i = 0; i < 6; i++)
             {
-                if (data.GetNeighbor(x, y, z, (Direction) i) == null)
-                    MakeFace((Direction)i, scale, cubePos);
+                if (data.GetNeighbor(coordinate, (Direction) i) == null)
+                    MakeFace((Direction) i, scale, cubePos);
             }
         }
 
